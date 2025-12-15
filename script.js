71a0c6fe575eedbb1770afdf32b8634abe01c396
DivyaM2496 <srilalithakiranmai@gmail.com>
2025-12-15 17:06:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+function change1() {
+    let presentColor = document.getElementById("button1").textContent;
+    document.getElementById("selectedColorHexCode").textContent = presentColor;
+    document.getElementById("colorPickerContainer").style.backgroundColor = presentColor;
+
+}
+
+function change2() {
+    let presentColor = document.getElementById("button2").textContent;
+    document.getElementById("selectedColorHexCode").textContent = presentColor;
+    document.getElementById("colorPickerContainer").style.backgroundColor = presentColor;
+
+
+}
+
+function change3() {
+    let presentColor = document.getElementById("button3").textContent;
+    document.getElementById("selectedColorHexCode").textContent = presentColor;
+    document.getElementById("colorPickerContainer").style.backgroundColor = presentColor;
+
+}
+
+function change4() {
+    let presentColor = document.getElementById("button4").textContent;
+    document.getElementById("selectedColorHexCode").textContent = presentColor;
+    document.getElementById("colorPickerContainer").style.backgroundColor = presentColor;
+
+}
